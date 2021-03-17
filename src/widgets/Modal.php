0e05f0b7504701db31e30488a59372519e4dfcd7
Lisman Sihotang <lisman.sihotang@gmail.com>
2021-03-18 01:18:48 +0700

    fixed

---
+++ src/widgets/Modal.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace lismansihotang\yii2tabler\src\widgets;
+
+
+use Yii;
+use yii\bootstrap\Widget;
+
+class Modal extends Widget
+{
+    public function run()
+    {
+        echo "Modal";
+    }
+}
