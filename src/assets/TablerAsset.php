ac1e76e109f3f5ad0ef328c08d873e3f5f3f93e7
Lisman Sihotang <lisman.sihotang@gmail.com>
2021-03-17 23:17:16 +0700

    added asset and themes tabler

---
+++ src/assets/TablerAsset.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class TablerAsset extends AssetBundle
+{
+    public $basePath = '@webroot/themes/tabler/dist';
+    public $baseUrl = '@web/themes/tabler/dist';
+    public $css = [
+        //'css/site.css',
+        'css/tabler.min.css'
+    ];
+    public $js = [
+        'js/tabler.min.js'
+    ];
+    public $depends = [
+        //'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapPluginAsset',
+    ];
+}
